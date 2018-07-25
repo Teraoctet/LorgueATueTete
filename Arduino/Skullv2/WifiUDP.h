56fb2739f89da6b9ea1ec42aed592055e49d514f
COSTES Antoine <antoine.costes@irisa.fr>
2018-07-25 16:14:26 +0200

    clean

--- Arduino/Skullv2/WifiUDP.h
+++
@@ -1,5 +0,0 @@
-void wifi_setup();
-void  wifi_routeMessages();
-void wifi_sendPing();
-void wifi_handshake(OSCMessage &msg, int addrOffset);
-void wifi_sendOSCMessage(OSCMessage &msg);
--- Arduino/Skullv3_Jack/Leds.h
+++
@@ -1,5 +0,0 @@
-void leds_roar();
-//void leds_set(int index, int r, int g, int b);
-void leds_setAll(int r, int g , int b, int t);
-void leds_blinkAll(int r, int g , int b, int nb);
-void leds_flash();
